f93db31903a7d02f3c385db8ac0f2457181e40c4
Tiago Braga <braga.tiagoxavier@gmail.com>
2022-08-04 20:58:20 -0300

    Create interface grafic

--- app.js
+++ app.js
@@ -1,28 +1,31 @@
-const binary = '1010231';
+const binary = '110011001';
 
 // error in valid binary number
 
 function bin2dec(bin){
     let decimal = 0;
     let len_binary = bin.length - 1;
-    
-    for(let i = 0; i <= (bin.length - 1); i++){
-        if(bin.length[i] > 1){
-            alert("Number invalid");
-            console.log("Invalid")
+
+
+    if(bin.length <= 8){
+
+        for(let i = 0; i <= (bin.length - 1); i++){
+        if(bin[i] > 1){
+            console.log('Number invalid');
+            break;
         }
         else{
-           /* for(let i = 0; len_binary >= 0; i++){
-                decimal += eval(bin[len_binary] * (2 ** i)); 
+            for(let j= 0; len_binary >= 0; j++){
+                decimal += eval(bin[len_binary] * (2 ** j));
                 len_binary--;
-            }*/
-            return decimal;
+                }
+            }
         }
-        console.log("Erro")
+    } else {
+        console.log('Number invalid');
     }
 
-    
+    return decimal;
+
 }
-
-
 console.log(bin2dec(binary))
